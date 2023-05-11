0bbe639086ae456c8fa3c4ef3df9925b8b9125cc
JoabeConrado19 <joabe.conrado19@gmail.com>
2023-05-11 12:05:13 -0400

    Fix: portifolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,7 +1,7 @@
 export const userData = {
   nameUser: "Joabe Conrado",
   githubUser: "JoabeConrado19",
-  linkedinUser: "Joabe Conrado Borges Cavalcante",
+  linkedinUser: "joabe-conrado-borges-cavalcante-b77919218",
   whatsappNumber: "+5592994493565",
   emailUser: "joabe.conrado19@gmail.com",
   instagramUser: "",
